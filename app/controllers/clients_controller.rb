c022723d3bace61d339d9a5a5acae5d349c59d72
Pedro P Camellon <pedropcamellon@gmail.com>
2024-05-11 22:42:48 -0400

    ClientsController. Get user before index, :create, :show, :edit, :update and destroy. Get client before show, :edit, :update, and destroy.

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,63 +1,62 @@
 class ClientsController < ApplicationController
-    def index
-      user = User.find_by(id: session[:user_id])
-      @clients = user.clients
+  before_action :find_user, only: [:index, :create, :show, :edit, :update, :destroy]
+  before_action :find_client, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @clients = @user.clients
+  end
+
+  def new
+    @client = Client.new
+    render layout: false
+  end
+
+  def create
+    @client = @user.clients.new(client_params)
+
+    if @client.save
+      flash[:success] = 'Customer profile created.'
+      redirect_to "/users/#{@user.id}/clients/#{@client.id}", status: 200
+    else
+      flash[:info] = @client.errors.full_messages.first
+      render 'new'
     end
-  
-    def new
-      @client = Client.new
-      render layout: false
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @client.update(client_params)
+      flash[:success] = 'Customer profile updated.'
+      redirect_to @client
+    else
+      flash[:danger] = 'Unable to update profile.'
+      render 'edit'
     end
-  
-    def create
-      user = User.find_by(id: session[:user_id])
-      @client = user.clients.new(client_params)
-  
-      if @client.save
-        flash[:success] = 'Customer profile created.'
-        redirect_to "/users/#{user.id}/clients/#{@client.id}", status: 200
-      else
-        flash[:info] = @client.errors.full_messages.first
-        render 'new'
-      end
+  end
+
+  def destroy
+    if @client
+      @client.destroy
+      redirect_to @user.clients
     end
-  
-    def show
-      user = User.find_by(id: session[:user_id])
-      @client = user.clients.find_by(id: params[:id])
-    end
-  
-    def edit
-      user = User.find_by(id: session[:user_id])
-      @client = user.clients.find_by(id: params[:id])
-    end
-  
-    def update
-      user = User.find_by(id: session[:user_id])
-      @client = user.clients.find_by(id: params[:id])
-  
-      if @client.update(client_params)
-        flash[:success] = 'Customer profile updated.'
-        redirect_to @client
-      else
-        flash[:danger] = 'Unable to update profile.'
-        render 'edit'
-      end
-    end
-  
-    def destroy
-      user = User.find_by(id: session[:user_id])
-  
-      if @client = user.clients.find_by(id: params[:id])
-        @client.destroy
-        redirect_to user.clients
-      end
-    end
-  
-    private
-  
+  end
+
+  private
+
     def client_params
       params.require(:client).permit(:company_name, :value)
     end
-  end
-  
+
+    def find_user
+      @user = User.find_by(id: session[:user_id])
+    end
+
+    def find_client
+      @client = @user.clients.find_by(id: params[:id])
+    end
+end
