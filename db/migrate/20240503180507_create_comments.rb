c3a6c15d77f365118a3bfb6faaa44fa8fda99038
Pedro P Camellon <pedropcamellon@gmail.com>
2024-05-13 14:40:54 +0000

    feat: Fix migration file create_comments.rb to correct syntax for t.string and t.text and add migration file add_client_ref_to_comments.rb to create a reference to client in comments

--- db/migrate/20240503180507_create_comments.rb
+++ db/migrate/20240503180507_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
-      t.string: :title
-      t.text: :body
+      t.string :title
+      t.text :body
       t.references :client, foreign_key: true, null: false
       t.timestamps
     end
--- db/migrate/20240511160743_add_client_ref_to_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddClientRefToComments < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :comments, :client, null: false, foreign_key: true
-  end
-end
