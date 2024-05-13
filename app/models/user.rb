7971fcbcca51b6e8a43f5fd4b723b250d2b0af9b
PP <pedropcamellon@gmail.com>
2024-05-13 16:04:37 +0000

    feat: Add logging for new user registration in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
 class User < ApplicationRecord
+    after_create :log_new_user
+
     validates :first_name, :last_name, :company, :email, :password_digest, presence: true
     validates :password_digest, length: { minimum: 6 }
     validates :email, uniqueness: true
   
     has_many :clients
     has_secure_password
-  end
+
+    private
+      def log_new_user
+        puts "A new user was registered"
+      end
+end
   
