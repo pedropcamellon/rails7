3c4b5e7afc2839db2b45c30cfe9af2eb94bac95c
Pedro P Camellon <pedropcamellon@gmail.com>
2024-05-13 14:40:38 +0000

    feat: Remove X-Frame-Options header in allow_iframe method and add 'gnrpyq-3000.csb.app'

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Rails7
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.hosts << "gnrpyq-3000.csb.app"
   end
 end
