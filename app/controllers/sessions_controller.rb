871f5f3b117a15724db0018d832006ea41ab730f
PP <pedropcamellon@gmail.com>
2024-05-14 19:01:33 +0000

    UsersController. Ensure user is logged in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,28 +1,27 @@
 class SessionsController < ApplicationController
-    def login
-      @user = User.new
-    end
-  
-    def authenticate
-      if @user = User.find_by(email: params[:email]).try(:authenticate, params[:password])
-        session[:user_id] = @user.id
-        redirect_to "/users/#{@user.id}/clients"
-      else
-        flash[:danger] = 'Not a valid email or password.'
-        render 'login'
-      end
-    end
-  
-    def logout
-      session.clear
-      redirect_to '/'
-    end
-  
-    private
-  
-    def session_params
-      params.permit(:email, :password)
-    end
-  
+  def login
+    @user = User.new
   end
+
+  def authenticate
+    if @user = User.find_by(email: params[:email]).try(:authenticate, params[:password])
+      session[:user_id] = @user.id
+      redirect_to "/users/#{@user.id}/clients"
+    else
+      flash[:danger] = 'Not a valid email or password.'
+      render 'login'
+    end
+  end
+
+  def logout
+    session.clear
+    redirect_to '/'
+  end
+
+  private
+
+  def session_params
+    params.permit(:email, :password)
+  end
+end
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,45 +1,62 @@
 class UsersController < ApplicationController
-    def new
-      @user = User.new
+  before_action :require_login, only: [:show, :edit, :update]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "Thanks for signing up, #{@user.first_name}!"
+      redirect_to @user
+    else
+      flash[:info] = @user.errors.full_messages.first
+      render 'new'
     end
-  
-    def create
-      @user = User.new(user_params)
-  
-      if @user.save
-        session[:user_id] = @user.id
-        flash[:success] = "Thanks for signing up, #{@user.first_name}!"
-        redirect_to @user
-      else
-        flash[:info] = @user.errors.full_messages.first
-        render 'new'
-      end
+  end
+
+  def show
+    @user = User.find_by(id: session[:user_id])
+  end
+
+  def edit
+    @user = User.find_by(id: session[:user_id])
+  end
+
+  def update
+    @user = User.find_by(id: session[:user_id])
+
+    if @user.update(user_params)
+      flash[:success] = 'Profile Updated'
+      redirect_to @user
+    else
+      flash[:danger] = 'Unable to update profile.'
+      render 'edit'
     end
-  
-    def show
-      @user = User.find_by(id: session[:user_id])
-    end
-  
-    def edit
-      @user = User.find_by(id: session[:user_id])
-    end
-  
-    def update
-      @user = User.find_by(id: session[:user_id])
-  
-      if @user.update(user_params)
-        flash[:success] = 'Profile Updated'
-        redirect_to @user
-      else
-        flash[:danger] = 'Unable to update profile.'
-        render 'edit'
-      end
-    end
-  
-    private
-  
+  end
+
+  private
+
     def user_params
       params.require(:user).permit(:first_name, :last_name, :company, :email, :password, :password_confirmation)
     end
-  end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+      !!current_user
+    end
+
+    def require_login
+      unless logged_in?
+        flash[:error] = 'You must be logged in to access this page.'
+        redirect_to login_url
+      end
+    end
+end
   
